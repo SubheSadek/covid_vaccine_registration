0396aeef227660c0f13cdb5009908011e044146c
subhesadek <subhesadek89990@gmail.com>
2023-03-19 23:26:14 +0600

    Add data type casting to functions

--- app/Http/Controllers/VaccineRegistration/VaccineRegistrationController.php
+++ app/Http/Controllers/VaccineRegistration/VaccineRegistrationController.php
@@ -41,12 +41,12 @@ class VaccineRegistrationController extends Controller
         try {
             $requestData['scheduled_date'] = $this->regService->getNextAvailableDateForCenter((int) $requestData['vaccine_center_id']);
             $registration = VaccineRegistration::create($requestData);
-            $emailData = $this->regService->emailData($registration);
 
             // We're creating a Carbon instance with the vaccination date, subtracting one day, setting the time to 9 PM,
             // and converting it to the Bangladesh time zone. We're then queuing a new instance of the VaccineNotification
             // mail class to be sent to the user at the specified notification time.
 
+            $emailData = $this->regService->emailData((object) $registration);
             $notification_time = Carbon::parse($emailData['scheduled_date'])->subDay()->setTimezone('Asia/Dhaka')->setTime(21, 0, 0);
             Mail::to($emailData['email'])->later($notification_time, new VaccineNotification($emailData));
             DB::commit();
